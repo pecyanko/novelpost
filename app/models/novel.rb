e6164c2a976b56399641bb28468b10af6f6caccc
pecyanko <psar.xx@icloud.com>
2023-02-07 18:01:38 +0900

    一覧表示

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,2 +1,13 @@
 class Novel < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :review, presence: true
+
+
+  validates :category_id, numericality: { other_than: 1, message: "を入力してください" }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "novels#index"
-  resources :novels, only:[:index, :new]
+  resources :novels, only:[:index, :new, :create]
 end
